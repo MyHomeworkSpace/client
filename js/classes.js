868e1caa6dd12e9ac2afa905cbf473ef89d13f3b
Alex Studer <alex_studer@outlook.com>
2016-09-10 23:21:38 -0400

    editable classes

--- js/classes.js
+++ js/classes.js
@@ -1,25 +1,36 @@
 MyHomeworkSpace.Pages.classes = {
 	init: function() {
 		$("#addClass").click(function() {
+			$("#classModal").attr("data-actionType", "add");
 			$("#classModalType").text("Add");
 			$("#classModal").modal();
 		});
 		$("#submitClassModal").click(function() {
+			var type = $("#classModal").attr("data-actionType");
+			var id = $("#classModal").attr("data-actionId");
 			$("#classModal").modal("hide");
 			$("#loadingModal").modal({
 				backdrop: "static",
 				keyboard: false
 			});
-			MyHomeworkSpace.API.post("classes/add", {
+			var classItem = {
 				name: $("#className").val(),
 				teacher: $("#classTeacher").val()
-			}, function(xhr) {
+			};
+			var classPostDone = function(xhr) {
 				MyHomeworkSpace.Classes.load(function() {
 					MyHomeworkSpace.Classes.reload();
 					$("#loadingModal").modal("hide");
 					MyHomeworkSpace.Page.show("classes");
 				});
-			});
+			};
+
+			if (type == "add") {
+				MyHomeworkSpace.API.post("classes/add", classItem, classPostDone);
+			} else {
+				classItem.id = id;
+				MyHomeworkSpace.API.post("classes/edit", classItem, classPostDone);
+			}
 		});
 	},
 	open: function() {
@@ -27,12 +38,26 @@ MyHomeworkSpace.Pages.classes = {
 		for (var classIndex in MyHomeworkSpace.Classes.list) {
 			var classItem = MyHomeworkSpace.Classes.list[classIndex];
 			var $item = $('<li class="classItem"></li>');
+				$item.attr("data-classId", classItem.id);
 				var $name = $('<div class="className"></div>');
 					$name.text(classItem.name);
 				$item.append($name);
 				var $teacher = $('<div class="classTeacher"></div>');
 					$teacher.text(classItem.teacher);
 				$item.append($teacher);
+				var $edit = $('<i class="fa fa-edit"></i>');
+					$edit.click(function() {
+						var $that = $(this);
+						MyHomeworkSpace.API.get("classes/get/" + $(this).parent().attr("data-classId"), {}, function(xhr) {
+							var classItem = xhr.responseJSON.class;
+							$("#classModal").attr("data-actionType", "edit");
+							$("#classModal").attr("data-actionId", $that.parent().attr("data-classId"));
+							$("#className").val(classItem.name);
+							$("#classTeacher").val(classItem.teacher);
+							$("#classModal").modal();
+						});
+					});
+				$item.append($edit);
 			$(".classList ul").append($item);
 		}
 	}
