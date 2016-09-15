b6beeacc9628ba0a6dffac63fe58325c00bf7e42
Alex Studer <alex_studer@outlook.com>
2016-09-15 13:23:00 -0400

    add swapping classes

--- js/classes.js
+++ js/classes.js
@@ -74,21 +74,59 @@ MyHomeworkSpace.Pages.classes = {
 				var $teacher = $('<div class="classTeacher"></div>');
 					$teacher.text(classItem.teacher);
 				$item.append($teacher);
-				var $edit = $('<i class="fa fa-edit"></i>');
-					$edit.click(function() {
-						var $that = $(this);
-						MyHomeworkSpace.API.get("classes/get/" + $(this).parent().attr("data-classId"), {}, function(xhr) {
-							var classItem = xhr.responseJSON.class;
-							$("#deleteClassModal").show();
-							$("#classModal").attr("data-actionType", "edit");
-							$("#classModal").attr("data-actionId", $that.parent().attr("data-classId"));
-							$("#className").val(classItem.name);
-							$("#classTeacher").val(classItem.teacher);
-							$("#classModalType").text("Edit");
-							$("#classModal").modal();
-						});
+			var $swap = $('<i class="fa fa-arrows-v"></i>');
+				$swap.click(function() {
+					var $that = $(this);
+					var currentId = $(this).parent().attr("data-classId");
+					MyHomeworkSpace.API.get("classes/get/" + currentId, {}, function(xhr) {
+						$("#classSwapOptions").text("");
+						$("#classSwapModal").attr("data-classId", currentId);
+						var classItem = xhr.responseJSON.class;
+						for (var classIndex in MyHomeworkSpace.Classes.list) {
+							var thisClassItem = MyHomeworkSpace.Classes.list[classIndex];
+							if (thisClassItem.id == currentId) {
+								continue;
+							}
+							var $classItem = $('<li class="classSwapItem"></li>');
+								$classItem.attr("data-classId", thisClassItem.id);
+								$classItem.text(thisClassItem.name);
+								$classItem.click(function() {
+									var oldId = $("#classSwapModal").attr("data-classId");
+									var newId = $(this).attr("data-classId");
+									$("#classSwapModal").modal('hide');
+									$("#loadingModal").modal({
+										backdrop: "static",
+										keyboard: false
+									});
+									MyHomeworkSpace.API.post("classes/swap", {
+										id1: oldId,
+										id2: newId
+									}, function(response) {
+										MyHomeworkSpace.Pages.classes.handleNew();
+									});
+								});
+							$("#classSwapOptions").append($classItem);
+						}
+						$(".classSwapName").text(classItem.name);
+						$("#classSwapModal").modal();
 					});
-				$item.append($edit);
+				});
+			$item.append($swap);
+			var $edit = $('<i class="fa fa-edit"></i>');
+				$edit.click(function() {
+					var $that = $(this);
+					MyHomeworkSpace.API.get("classes/get/" + $(this).parent().attr("data-classId"), {}, function(xhr) {
+						var classItem = xhr.responseJSON.class;
+						$("#deleteClassModal").show();
+						$("#classModal").attr("data-actionType", "edit");
+						$("#classModal").attr("data-actionId", $that.parent().attr("data-classId"));
+						$("#className").val(classItem.name);
+						$("#classTeacher").val(classItem.teacher);
+						$("#classModalType").text("Edit");
+						$("#classModal").modal();
+					});
+				});
+			$item.append($edit);
 			$(".classList ul").append($item);
 		}
 	}
