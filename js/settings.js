5a7370890f42802375d3437040a7a027ba321360
Alex Studer <alex_studer@outlook.com>
2016-11-13 00:02:24 -0500

    add background gallery

--- js/settings.js
+++ js/settings.js
@@ -26,10 +26,33 @@ MyHomeworkSpace.Pages.settings = {
 
 			});
 		});
+		$("#settings_change_background").click(function() {
+			$("#changeBackgroundModal").modal();
+		});
+		$(".backgroundOption").click(function() {
+			$("body").css("background-image", "url(img/backgrounds/bg" + $(this).attr("data-bgIndex") + ".jpg)");
+			MyHomeworkSpace.API.post("prefs/set", {
+				key: "background",
+				value: "img:" + $(this).attr("data-bgIndex")
+			}, function(xhr) {
+
+			});
+		});
 	},
 	open: function() {
 		$("#settings_account_name").text(MyHomeworkSpace.Me.name);
 		$("#settings_account_email").text(MyHomeworkSpace.Me.email);
+		MyHomeworkSpace.API.get("prefs/get/background", {}, function(xhr) {
+			if (xhr.responseJSON.status != "error") {
+				var bgVal = xhr.responseJSON.pref.value;
+				MyHomeworkSpace.Pages.settings.cache["background"] = bgVal;
+				var bgType = bgVal.split(":")[0];
+				var bgVal = bgVal.split(":")[1];
+				if (bgType == "img") {
+					$("body").css("background-image", "url(img/backgrounds/bg" + bgVal + ".jpg)");
+				}
+			}
+		});
 	},
 	cache: {}
 };
