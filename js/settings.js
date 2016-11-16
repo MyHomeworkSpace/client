d547c09839f8a67b749a79a7b136474062db45d3
Alex Studer <alex_studer@outlook.com>
2016-11-15 20:35:49 -0500

    close #36

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,6 @@
 MyHomeworkSpace.Pages.settings = {
+	cache: {},
+	hackyBackgroundColorTimeout: undefined,
 	init: function() {
 
 	},
@@ -7,10 +9,9 @@ MyHomeworkSpace.Pages.settings = {
 			if (xhr.responseJSON.status != "error") {
 				var bgVal = xhr.responseJSON.pref.value;
 				MyHomeworkSpace.Pages.settings.cache["background"] = bgVal;
-				var bgType = bgVal.split(":")[0];
-				var bgVal = bgVal.split(":")[1];
-				if (bgType == "img") {
-					$("body").css("background-image", "url(img/backgrounds/bg" + bgVal + ".jpg)");
+				MyHomeworkSpace.Pages.settings.setBackground(bgVal);
+				if (bgVal.substr(0, 3) == "clr") {
+					$("#backgroundColor").val(bgVal.split(":")[1]);
 				}
 			}
 		});
@@ -45,7 +46,7 @@ MyHomeworkSpace.Pages.settings = {
 			$("#changeBackgroundModal").modal();
 		});
 		$(".backgroundOption").click(function() {
-			$("body").css("background-image", "url(img/backgrounds/bg" + $(this).attr("data-bgIndex") + ".jpg)");
+			MyHomeworkSpace.Pages.settings.setBackground("img:" + $(this).attr("data-bgIndex"));
 			MyHomeworkSpace.API.post("prefs/set", {
 				key: "background",
 				value: "img:" + $(this).attr("data-bgIndex")
@@ -53,10 +54,56 @@ MyHomeworkSpace.Pages.settings = {
 
 			});
 		});
+		$("#backgroundColor").change(function() {
+			if (MyHomeworkSpace.Pages.settings.hackyBackgroundColorTimeout !== undefined) {
+				clearTimeout(MyHomeworkSpace.Pages.settings.hackyBackgroundColorTimeout);
+			}
+
+			var newBg = "clr:" + $(this).val();
+			MyHomeworkSpace.Pages.settings.setBackground(newBg);
+
+			// to avoid flooding the server with network traffic, use a timeout
+			// the pref will only be saved if the user doesn't change the color for 200ms
+			MyHomeworkSpace.Pages.settings.hackyBackgroundColorTimeout = setTimeout(function() {
+				MyHomeworkSpace.API.post("prefs/set", {
+					key: "background",
+					value: newBg
+				}, function(xhr) {
+
+				});
+			}, 300);
+		});
 	},
 	open: function() {
 		$("#settings_account_name").text(MyHomeworkSpace.Me.name);
 		$("#settings_account_email").text(MyHomeworkSpace.Me.email);
 	},
-	cache: {}
+	setBackground: function(bgVal) {
+		var bgType = bgVal.split(":")[0];
+		var bgVal = bgVal.split(":")[1];
+		if (bgType == "img") {
+			$("body").css("background-image", "url(img/backgrounds/bg" + bgVal + ".jpg)");
+			$("#topBar").removeClass("inverted");
+			$("#sidebar").removeClass("inverted");
+		} else if (bgType == "clr") {
+			$("body").css("background-image", "none");
+			$("body").css("background-color", bgVal);
+
+			// text adjustment code
+			// this will check if the color is bright, and if it is, invert the color of the tabs and navbar
+			var newClr = bgVal.substr(1); // we are assuming it's in format #rrggbb
+			// convert hex code into numbers
+			var red = parseInt(newClr.substr(0, 2), 16);
+			var green = parseInt(newClr.substr(2, 2), 16);
+			var blue = parseInt(newClr.substr(4, 2), 16);
+
+			if (red > 128 || green > 128 || blue > 128) {
+				$("#topBar").addClass("inverted");
+				$("#sidebar").addClass("inverted");
+			} else {
+				$("#topBar").removeClass("inverted");
+				$("#sidebar").removeClass("inverted");
+			}
+		}
+	}
 };
