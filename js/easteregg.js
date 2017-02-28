3e0c8d30ab12370af3d955bfbf936b4bc2454374
William B <william@barkoffusa.com>
2017-02-28 12:41:45 -0500

    :)

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,7 +1,27 @@
 function loadclippy() {
-	clippy.load('Clippy', function(agent){
+    document.getElementById("clipify").style.display = 'none';
+    clippy.load('Clippy', function (agent) {
         agent.play('Greeting');
         agent.speak('When all else fails, bind some paper together. My name is Clippy.');
         agent.show();
-	})
-}l
+    })
+     document.getElementById("addHWBtn").addEventListener("click", function () {
+            agent.speak("Type your assignment here.");
+            agent.play('Writing');
+        });
+        document.getElementById("submitHomeworkModal").addEventListener("click", function () {
+            agent.play('Save');
+            agent.speak("Assignment Added");
+        });
+        document.getElementById("settings_change_background").addEventListener("click", function () {
+            agent.play('GetArtsy');
+        });
+        document.getElementById("submitFeedbackModal").addEventListener("click", function () {
+            agent.play('SendMail');
+            agent.speak("Your feedback has been sent.");
+        });
+        document.getElementById("deleteHomeworkModal").addEventListener("click", function () {
+            agent.play('EmptyTrash');
+            agent.speack("Your assignment has been deleted");
+        })
+}
