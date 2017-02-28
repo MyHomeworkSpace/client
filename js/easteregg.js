f1801323be0ebe06dfa3d6dc1b52baa558819c49
William B <william@barkoffusa.com>
2017-02-28 16:16:17 -0500

    More Clippy

--- js/easteregg.js
+++ js/easteregg.js
@@ -4,8 +4,7 @@ function loadclippy() {
         agent.play('Greeting');
         agent.speak('When all else fails, bind some paper together. My name is Clippy.');
         agent.show();
-    })
-     document.getElementById("addHWBtn").addEventListener("click", function () {
+        document.getElementById("addHWBtn").addEventListener("click", function () {
             agent.speak("Type your assignment here.");
             agent.play('Writing');
         });
@@ -22,6 +21,22 @@ function loadclippy() {
         });
         document.getElementById("deleteHomeworkModal").addEventListener("click", function () {
             agent.play('EmptyTrash');
-            agent.speack("Your assignment has been deleted");
+            agent.speak("Your assignment has been deleted");
         })
+        document.getElementById("feedbackSmile").addEventListener("click", function() {
+            agent.play("Congratulate");
+            agent.speak("I'm glad you're happy with MyHomeworkSpace! Use this form to tell us what you like.");
+        })
+        document.getElementById("feedbackFrown").addEventListener("click", function () {
+            agent.play("GetAttention");
+            agent.speak("I'm sorry if you are encountering issues. Use this form to tell us what you don't like.");
+        });
+        document.getElementById("feedbackIdea").addEventListener("click", function () {
+            agent.play("GetTechy");
+            agent.speak("Have an idea for a feature? Use this form to tell us what you want to see next.");
+        });
+        document.getElementsByClassName("sidebarItem").addEventListener("click", function () {
+            agent.animate();
+        });
+    })
 }
