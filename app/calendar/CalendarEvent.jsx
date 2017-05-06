70dff2db90cd668c6cc4cc803990294f7813d4c8
Alex Studer <alex_studer@outlook.com>
2017-05-06 19:21:13 -0400

    formatting of calendar items

--- app/calendar/CalendarEvent.jsx
+++ app/calendar/CalendarEvent.jsx
@@ -5,15 +5,28 @@ import linkState from "linkstate";
 
 class CalendarEvent extends Component {
 	render(props, state) {
-		var dayStart = moment.unix(0);
-		var start = moment.unix(props.item.start);
-		var end = moment.unix(props.item.end);
+		var dayStart = moment.unix(0).utc();
+		var start = moment.unix(props.item.start).utc();
+		var end = moment.unix(props.item.end).utc();
 
 		var offset = start.diff(dayStart, "minutes");
 		var durationInMinutes = end.diff(start, "minutes");
 
+		var startDisplay = start.format("h:MM a");
+		var endDisplay = end.format("h:MM a");
+
+		var isScheduleItem = (props.type == "schedule");
+
+		var displayName = props.item.name;
+
+		if (isScheduleItem) {
+			displayName = displayName.split("-")[0].trim().split(":")[0].trim();
+		}
+
 		return <div class="calendarEvent" style={`top: ${offset}px; height: ${durationInMinutes}px`}>
-			{props.item.name}
+			<div class="calendarEventName">{displayName}</div>
+			{isScheduleItem && <div class="calendarEventTeacher">{props.item.ownerName}</div>}
+			<div class="calendarEventTime">{startDisplay} to {endDisplay}</div>
 		</div>;
 	}
 }
