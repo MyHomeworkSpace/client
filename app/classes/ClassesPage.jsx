3ec9d482bbc1e3bc8f7e5c571d3103f47a9ed139
Alex Studer <astuder@notetoscreen.com>
2018-10-18 09:49:16 -0400

    classes page does not need to make its own request for the classes list, but can use the existing one

--- app/classes/ClassesPage.jsx
+++ app/classes/ClassesPage.jsx
@@ -2,56 +2,29 @@ import "classes/ClassesPage.styl";
 
 import { h, Component } from "preact";
 
-import api from "api.js";
-
 import AddClass from "classes/AddClass.jsx";
 import ClassDetails from "classes/ClassDetails.jsx";
 import ClassList from "classes/ClassList.jsx";
 
 class ClassesPage extends Component {
-	constructor() {
-		super();
-		this.state = {
-			loading: true
-		};
-	}
-
-	componentDidMount() {
-		this.load();
-	}
-
-	componentWillReceiveProps(nextProps, nextState) {
-		this.load();
-	}
-	
-	load() {
-		this.setState({
-			loading: true
-		}, function() {
-			var that = this;
-			api.get("classes/get", {
-				showToday: true
-			}, function(data) {
-				that.setState({
-					loading: false,
-					classes: data.classes,
-					selectedClass: null
-				});
-			});
-		});
-	}
-
 	selectClass(newClass) {
 		this.setState({
-			selectedClass: newClass
+			selectedClassID: newClass.id
 		});
 	}
 	
 	render(props, state) {
-		if (state.loading) {
-			return <div class="classesPage">
-				Loading, please wait...
-			</div>;
+		var classes = MyHomeworkSpace.Classes.list;
+
+		var selectedClass;
+
+		if (state.selectedClassID) {
+			classes.forEach(function(classObject) {
+				if (classObject.id == state.selectedClassID) {
+					selectedClass = classObject;
+					return false;
+				}
+			});
 		}
 
 		return <div class="classesPage">
@@ -59,11 +32,11 @@ class ClassesPage extends Component {
 				<div class="col-md-3 classesList">
 					<h2>Classes</h2>
 					<p class="lead">Select a class to view homework and details</p>
-					<ClassList classes={state.classes} onClick={this.selectClass.bind(this)}/>
-					<AddClass openModal={props.openModal}/>
+					<ClassList classes={classes} onClick={this.selectClass.bind(this)} />
+					<AddClass openModal={props.openModal} />
 				</div>
 				<div class="col-md-9">
-					{state.selectedClass == null ? <p class="lead noClassSelected">Select a class for details</p> : <ClassDetails classObject={state.selectedClass} openModal={props.openModal} />}
+					{selectedClass ? <ClassDetails classObject={selectedClass} openModal={props.openModal} /> : <p class="lead noClassSelected">Select a class for details</p>}
 				</div>
 			</div>
 		</div>;
