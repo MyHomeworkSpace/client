f652bc27f69619ff7978c6a450933c7705583236
William B <william@barkoffusa.com>
2019-03-09 13:51:06 -0500

    remove unused js

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,3 @@
-window.addEventListener("scroll",function() { 
-    if(window.scrollY > $("#keep-scrolling").offset().top && window.scrollY < $("#bottom-get-started").offset().top-20) {
-       $('#features-navbar').fadeIn();
-    }
-    else {
-       $('#features-navbar').fadeOut();
-    }
- },false);
-
 $('.marquee-left').marquee({
 	//duration in milliseconds of the marquee
 	duration: 10000,
@@ -17,8 +8,8 @@ $('.marquee-left').marquee({
 	//'left' or 'right'
 	direction: 'left',
 	//true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true,
- // pauseOnHover: true //don't need until we add popovers
+	duplicated: true,
+	// pauseOnHover: true //don't need until we add popovers
 });
 
 $('.marquee-right').marquee({
@@ -31,21 +22,6 @@ $('.marquee-right').marquee({
 	//'left' or 'right'
 	direction: 'right',
 	//true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true,
-    // pauseOnHover: true //don't need until we add popovers
+	duplicated: true,
+	// pauseOnHover: true //don't need until we add popovers
 });
-
-$(document).ready(function() {
-    if(document.location.href.indexOf("display-extra=finals") > -1) {
-		$("#finals").modal()
-	}
-});
-
-window.onload = function() {
-	var alert = document.querySelector("#whatsnew-alert");
-	if (alert) {
-		alert.addEventListener("click", function() {
-			$("#welcomeback").modal();
-		});
-	}
-};
