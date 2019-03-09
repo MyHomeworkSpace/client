b8834df010fa966bc8a3ce4e6717d80be4813338
William B <william@barkoffusa.com>
2019-03-09 13:51:18 -0500

    rewrite application auth for bootstrap 4

--- app/ui/LoginForm.jsx
+++ app/ui/LoginForm.jsx
@@ -43,13 +43,13 @@ export default class LoginForm extends Component {
 		this.setState({
 			loading: true,
 			error: ""
-		}, function() {
+		}, function () {
 			api.post("auth/login", {
 				username: that.state.username,
 				password: that.state.password
-			}, function(loginData) {
+			}, function (loginData) {
 				if (loginData.status == "ok") {
-					api.get("auth/me", {}, function(userData) {
+					api.get("auth/me", {}, function (userData) {
 						if (userData.status == "ok") {
 							that.props.callback.call(that, userData);
 						} else {
@@ -70,19 +70,37 @@ export default class LoginForm extends Component {
 	}
 
 	render(props, state) {
+		if (!this.props.bootstrap4) return <div class="loginForm">
+			<div class="loginFormTitle">Log in</div>
+			<p class="lead">Sign in using your Dalton account</p>
+			{state.error && <div class="alert alert-danger">{state.error}</div>}
+			<div class="input-group bs3">
+				<input type="text" class="form-control" placeholder="Username" onKeyup={this.keyup.bind(this)} onChange={linkState(this, "username")} value={state.username} disabled={state.loading} />
+				<span class="input-group-addon">@dalton.org</span>
+			</div>
+			<div class="input-group no-addon bs3">
+				<input type="password" class="form-control" placeholder="Password" onKeyup={this.keyup.bind(this)} onChange={linkState(this, "password")} value={state.password} disabled={state.loading} />
+			</div>
+			<button class="btn btn-lg btn-primary pull-right" onClick={this.login.bind(this)} disabled={state.loading}>
+				{state.loading ? <LoadingIndicator /> : "Log in"}
+			</button>
+			<div class="clearfix"></div>
+		</div>;
 		return <div class="loginForm">
 			<div class="loginFormTitle">Log in</div>
 			<p class="lead">Sign in using your Dalton account</p>
 			{state.error && <div class="alert alert-danger">{state.error}</div>}
-			<div class="input-group">
+			<div class="input-group mb-3">
 				<input type="text" class="form-control" placeholder="Username" onKeyup={this.keyup.bind(this)} onChange={linkState(this, "username")} value={state.username} disabled={state.loading} />
-				<span class="input-group-addon">@dalton.org</span>
+				<div class="input-group-append">
+					<span class="input-group-text" id="basic-addon2">@dalton.org</span>
+				</div>
 			</div>
 			<div class="input-group no-addon">
 				<input type="password" class="form-control" placeholder="Password" onKeyup={this.keyup.bind(this)} onChange={linkState(this, "password")} value={state.password} disabled={state.loading} />
 			</div>
-			<button class="btn btn-lg btn-primary pull-right" onClick={this.login.bind(this)} disabled={state.loading}>
-				{state.loading ? <LoadingIndicator /> : "Log in" }
+			<button class="btn btn-lg btn-primary pull-right bs4" onClick={this.login.bind(this)} disabled={state.loading}>
+				{state.loading ? <LoadingIndicator /> : "Log in"}
 			</button>
 			<div class="clearfix"></div>
 		</div>;
--- public/js/applicationAuth.js
+++ public/js/applicationAuth.js
@@ -1,21 +1,22 @@
 var currentApplication;
 
-var getApplicationID = function() {
+var getApplicationID = function () {
 	return unescape(window.location.href.match("\\?id=(.*?)(&|$)") ? window.location.href.match("\\?id=(.*?)(&|$)")[1] : "");
 };
 
-var getState = function() {
+var getState = function () {
 	return (window.location.href.match("&state=(.*)") ? window.location.href.match("&state=(.*)")[1] : "");
 };
 
-var afterLogin = function(info) {
+var afterLogin = function (info) {
 	MyHomeworkSpace.Me = info;
 
 	$(".applicationAuthLogin").addClass("hidden");
 	$(".applicationAuthLoadingOverlay").addClass("hidden");
 	$(".applicationAuthLoginInfo").removeClass("hidden");
 	$(".applicationAuthUser").text(MyHomeworkSpace.Me.name);
-	MyHomeworkSpace.API.get("application/get/" + getApplicationID(), {}, function(data) {
+	$(".applicationAuthFirstName").text(MyHomeworkSpace.Me.name.split(" ")[0]);
+	MyHomeworkSpace.API.get("application/get/" + getApplicationID(), {}, function (data) {
 		if (data.status == "ok") {
 			$(".applicationAuthRequest").removeClass("hidden");
 			currentApplication = data.application;
@@ -31,9 +32,9 @@ var afterLogin = function(info) {
 	});
 };
 
-$(document).ready(function() {
-	MyHomeworkSpace.API.init(function() {
-		MyHomeworkSpace.API.get("auth/me", {}, function(data) {
+$(document).ready(function () {
+	MyHomeworkSpace.API.init(function () {
+		MyHomeworkSpace.API.get("auth/me", {}, function (data) {
 			if (data.status == "ok") {
 				afterLogin(data);
 			} else {
@@ -44,16 +45,17 @@ $(document).ready(function() {
 	});
 
 	MHSBridge.default.render(MHSBridge.default.h(MHSBridge.default.ui.LoginForm, {
-		callback: function(data) {
+		callback: function (data) {
 			afterLogin(data);
-		}
+		},
+		bootstrap4: true
 	}), document.querySelector("#loginContainer"));
 
-	$("#applicationAuthAllow").click(function() {
+	$("#applicationAuthAllow").click(function () {
 		$(".applicationAuthLoadingOverlay").removeClass("hidden");
 		MyHomeworkSpace.API.post("application/completeAuth", {
 			clientId: getApplicationID()
-		}, function(data) {
+		}, function (data) {
 			if (data.status == "ok") {
 				window.location.href = currentApplication.callbackUrl + "?token=" + escape(data.token) + "&state=" + getState();
 			} else {
@@ -65,13 +67,14 @@ $(document).ready(function() {
 		});
 	});
 
-	$("#applicationAuthDeny").click(function() {
+	$("#applicationAuthDeny").click(function () {
 		window.location.pathname = "";
 	});
 
-	$("#applicationAuthLogout").click(function() {
+	$(".applicationAuthLogout").click(function (event) {
+		event.preventDefault();
 		$(".applicationAuthLoadingOverlay").removeClass("hidden");
-		MyHomeworkSpace.API.get("auth/logout", {}, function(data) {
+		MyHomeworkSpace.API.get("auth/logout", {}, function (data) {
 			window.location.reload();
 		});
 	});
