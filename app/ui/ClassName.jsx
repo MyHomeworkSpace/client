ae1cf1ac5f1358a71c6f41e1563c6d894357d9c1
Alex Studer <alex@studer.dev>
2019-05-17 11:57:50 -0400

    have ClassName gracefully handle undefined classes instead of just crashing

--- app/ui/ClassName.jsx
+++ app/ui/ClassName.jsx
@@ -2,7 +2,11 @@ import { h, Component } from "preact";
 
 class ClassName extends Component {
 	render(props, state) {
-		return <span class="className"><span style={`display:inline-block;width:12px;height:12px;border-radius:100%;margin-right:2px;background-color:#${props.classObject.color}`} /> {props.classObject.name}</span>;
+		var classObject = props.classObject || {
+			color: "000000",
+			name: "No class"
+		};
+		return <span class="className"><span style={`display:inline-block;width:12px;height:12px;border-radius:100%;margin-right:2px;background-color:#${classObject.color}`} /> {classObject.name}</span>;
 	}
 }
 
