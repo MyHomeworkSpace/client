d3edfdb63b6663cc7c348cac2deaf58973276297
Alex Studer <alex@studer.dev>
2019-05-22 23:33:23 -0400

    fix help page doing weird double scroll bar thing

--- app/help/HelpPage.jsx
+++ app/help/HelpPage.jsx
@@ -1,13 +1,15 @@
+import "help/HelpPage.styl";
+
 import { h, Component } from "preact";
 
-class HelpPage extends Component {
+export default class HelpPage extends Component {
 	render(props, state) {
-		return <div height="100%" style="height: calc(100vh - 100px);">
-			<h2>Help</h2>
-			<p class="lead">All support documentation is available at <a href="https://help.myhomework.space">help.myhomework.space</a>.</p>
-			<iframe src="https://support.myhomework.space/embedded-search" name="" scrolling="yes" marginHeight="0px" marginWidth="0px" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+		return <div class="helpPageContainer">
+			<div>
+				<h2>Help</h2>
+				<p class="lead">All support documentation is available at <a href="https://help.myhomework.space">help.myhomework.space</a>.</p>
+			</div>
+			<iframe src="https://support.myhomework.space/embedded-search" name="" scrolling="auto" marginHeight="0px" marginWidth="0px" allowFullScreen="" width="100%" frameBorder="0"></iframe>
 		</div>;
 	}
-}
-
-export default HelpPage;
+};
