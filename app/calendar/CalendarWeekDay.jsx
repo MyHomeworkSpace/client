96cfbcf91b4521bdcc5b853e80bdf9f0276b873a
Alex Studer <alex@studer.dev>
2019-05-22 23:59:00 -0400

    handle multiple calendar announcements on the same day, and correctly display calendar announcement borders

--- app/calendar/CalendarWeekDay.jsx
+++ app/calendar/CalendarWeekDay.jsx
@@ -4,22 +4,16 @@ import { h, Component } from "preact";
 
 class CalendarWeekDay extends Component {
 	render(props, state) {
-		var thisAnnouncement;
-		if (props.announcements) {
-			props.announcements.forEach(function(announcement) {
-				if (announcement.date == props.day.format("YYYY-MM-DD")) {
-					thisAnnouncement = announcement;
-				}
-			});
-		}
 		return <div class="calendarWeekDay">
 			<div class={`calendarWeekDayName ${props.day.isBefore(props.time, "day") ? "calendarWeekDayPast" : ""} ${props.day.isSame(props.time, "day") ? "calendarWeekDayToday" : ""}`}>
 				<span class="calendarWeekDayNameDow">{props.name}</span>
 				<span class="calendarWeekDayNameDate">{props.day.format("M/D")}</span>
 			</div>
-			{thisAnnouncement && <div class="calendarWeekDayAnnouncement">
-				{thisAnnouncement.text}
-			</div>}
+			{props.announcements && props.announcements.map(function(announcement) {
+				return <div class="calendarWeekDayAnnouncement">
+					{announcement.text}
+				</div>;
+			})}
 		</div>;
 	}
 }
