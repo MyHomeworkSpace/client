8213166b5312a61e13649784d23c8b1bb6ad7602
Alex Studer <alex@studer.dev>
2019-06-10 22:50:07 -0400

    handle new login error code and remove other random references to dalton

--- app/errors.js
+++ app/errors.js
@@ -3,7 +3,7 @@ var errorMap = {
 	"bb_no_grade": "We couldn't find what grade you're in. If you're a faculty member, this is a known issue that we're working to fix. If you're a student, please contact us with the Feedback button at the top-right.",
 	"bb_signin_error": "There was an error signing you in. Are you sure the password is correct?",
 	"bb_signin_rate_limit": "We're sorry, but schedule importing is currently unavailable. Try again in 30 minutes.",
-	"dalton_creds_incorrect": "The username or password was incorrect.",
+	"creds_incorrect": "The username or password was incorrect.",
 	"disconnected": "You're disconnected from the Internet. Try that again once you've reconnected.",
 	"missing_params": "Required parameters were missing from the request.",
 	"internal_server_error": "An internal server error occurred while processing the request.",
