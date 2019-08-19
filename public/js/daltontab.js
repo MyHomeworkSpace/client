ea259ae11e5c2fae958f359b2c3cf16649b0a200
Alex Studer <alex@studer.dev>
2019-08-19 17:56:45 -0400

    add required view beacon code to daltontab page, and clean up daltontab code

--- public/js/daltontab.js
+++ public/js/daltontab.js
@@ -1,46 +1,39 @@
-$().ready(() => {
+$(document).ready(function() {
     setClock();
-    setInterval(setClock,1000);
-    $.get("https://daltontabservices.myhomework.space/v1/getImage.php?channel=normal", (data) => {
+    setInterval(setClock, 1000);
+
+    $.get("https://daltontabservices.myhomework.space/v1/getImage.php?channel=normal", function(data) {
         bginfo = $.parseJSON(data);
+
         $(".dt").css("background-image", "url(" + bginfo.imgUrl + ")");
         $("#location").text(bginfo.description);
         $("#photographer").text(bginfo.authorName);
         $("#photographer").attr("href", bginfo.authorUrl);
         $("#source").text(bginfo.siteName);
-        $("#source").attr("href", bginfo.siteUrl)
-    })
-    if(navigator.userAgent.search("Chrome") > 1 ){
+        $("#source").attr("href", bginfo.siteUrl);
+
+        if (bginfo.beaconUrl) {
+            $.get(bginfo.beaconUrl, function(data) {
+                
+            });
+        }
+    });
+
+    if (navigator.userAgent.search("Chrome") != -1) {
         $("#download").html("Get DaltonTab <i class=\"fa fa-chrome\"></i>");
-        $("#download").attr("href", "https://chrome.google.com/webstore/detail/daltontab/ggfjkmflbbjndabmnngilkfpmdegbfkm")
-    } else if(navigator.userAgent.search("Firefox") > 1 ){
+        $("#download").attr("href", "https://chrome.google.com/webstore/detail/daltontab/ggfjkmflbbjndabmnngilkfpmdegbfkm");
+    } else if (navigator.userAgent.search("Firefox") != -1) {
         $("#download").html("Get DaltonTab <i class=\"fa fa-firefox\"></i>");
-        $("#download").attr("href", "https://addons.mozilla.org/en-US/firefox/addon/daltontab/")
+        $("#download").attr("href", "https://addons.mozilla.org/en-US/firefox/addon/daltontab/");
     } else {
-        $("#download").attr("download", "")
+        $("#download").hide();
     }
 })
 
 function setClock() {
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-    var d = new Date();
-    var hours = d.getHours()%12;
-    var am = (d.getHours < 12);
-    var minutes = padZero(d.getMinutes());
-    $("#hoursmins").text(hours + ":" + minutes);
-    $("#ampm").text((am ? "AM" : "PM"))
-    var month = monthNames[d.getMonth()];
-    var day = d.getDate();
-    var year = d.getFullYear();
-    $("#date").text(month + " " + day + ", " + year)
-}
+    var time = moment();
 
-function padZero(n) {
-    if(n == 0) {
-        return "00";
-    } else if(n > 10) {
-        return n;
-    } else { return "0" + n; }
+    $("#ampm").text(time.format("A"));
+    $("#hoursmins").text(time.format("h:mm"));
+    $("#date").text(time.format("MMMM Do, YYYY"));
 }
