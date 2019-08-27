bc0282c61a342490828eba1081731d11a7ec3c4c
Alex Studer <alex@studer.dev>
2019-08-26 22:52:24 -0400

    highlight current day in date picker, though it's somewhat ugly

--- app/ui/DatePickerCalendarGrid.jsx
+++ app/ui/DatePickerCalendarGrid.jsx
@@ -12,16 +12,21 @@ class DatePickerCalendarGrid extends Component {
 	render(props, state) {
 		var firstDay = moment(props.date).startOf("month");
 		var lastDay = moment(props.date).endOf("month");
+		var today = moment();
+
 		var items = [];
 		for (var i = 0; i < firstDay.day(); i++) {
 			// add spacers for days from last month
 			items.push(<div class="datePickerCalendarGridItem"></div>);
 		}
+
 		for (i = firstDay.date(); i <= lastDay.date(); i++) {
 			var thisDay = moment(props.date).date(i);
 			var isSelected = thisDay.isSame(props.currentDate, "day");
-			items.push(<div class={`datePickerCalendarGridItem datePickerCalendarGridItemDay ${isSelected ? "datePickerCalendarGridItemDaySelected" : ""}`} onClick={this.selectDate.bind(this, i)}>{i}</div>);
+			var isToday = thisDay.isSame(today, "day");
+			items.push(<div class={`datePickerCalendarGridItem datePickerCalendarGridItemDay ${isSelected ? "datePickerCalendarGridItemDaySelected" : ""} ${isToday ? "datePickerCalendarGridItemDayToday" : ""}`} onClick={this.selectDate.bind(this, i)}>{i}</div>);
 		}
+
 		return <div class="datePickerCalendarGrid">
 			<div class="datePickerCalendarGridItem">Sun</div>
 			<div class="datePickerCalendarGridItem">Mon</div>
