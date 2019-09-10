4fdf322e462a89cf0c9c844115b75b2f114cc0b7
Alex Studer <alex@studer.dev>
2019-09-10 12:31:37 -0400

    allow users to resend their verification emails

--- app/settings/panes/account/AccountPane.jsx
+++ app/settings/panes/account/AccountPane.jsx
@@ -2,8 +2,13 @@ import "settings/panes/account/AccountPane.styl";
 
 import { h, Component } from "preact";
 
+import api from "api.js";
+import errors from "errors.js";
+
 import SchoolList from "settings/panes/account/SchoolList.jsx";
 import TwoFactorInfo from "settings/panes/account/TwoFactorInfo.jsx";
+
+import LoadingIndicator from "ui/LoadingIndicator.jsx";
 
 export default class AccountPane extends Component {
 	changeBackground() {
@@ -22,12 +27,46 @@ export default class AccountPane extends Component {
 		this.props.openModal("enroll", {});
 	}
 
+	resendVerificationEmail() {
+		var that = this;
+
+		this.setState({
+			resendLoading: true
+		}, function() {
+			api.post("auth/resendVerificationEmail", {}, function(data) {
+				if (data.status == "ok") {
+					that.setState({
+						resendLoading: false,
+						resendSent: true
+					});
+				} else {
+					that.setState({
+						resendLoading: false,
+						error: errors.getFriendlyString(data.error)
+					});
+				}
+			});
+		});
+	}
+
 	render(props, state) {
 		return <div class="accountPane">
+			{state.error && <div class="alert alert-danger">{state.error}</div>}
+
 			<div class="accountGroup">
 				<div class="profile">
 					<h3 class="name">{props.me.name}</h3>
-					<h4 class="email">{props.me.email}</h4>
+					<h4 class="email">
+						{props.me.email}
+						{props.me.emailVerified ?
+							<span class="label label-success"><i class="fa fa-fw fa-check" /> Email verified</span> :
+							<span class="label label-warning">
+								{!state.resendLoading && !state.resendSent && <span><i class="fa fa-fw fa-exclamation-triangle" /> Email not verified, <a onClick={this.resendVerificationEmail.bind(this)}>resend verification email</a>?</span>}
+								{state.resendLoading && <span><LoadingIndicator /> Loading, please wait...</span>}
+								{state.resendSent && <span><i class="fa fa-fw fa-check" /> Verification email sent</span>}
+							</span>
+						}
+					</h4>
 				</div>
 				<button class="btn btn-primary" onClick={this.changeEmail.bind(this)}><i class="fa fa-fw fa-envelope" /> Change email</button>
 			</div>
