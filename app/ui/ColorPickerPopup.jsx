29e8e321947015e0cf775d1722e413fdc748ef77
William Barkoff <william@barkoffusa.com>
2020-04-30 23:05:55 -0400

    ui: allow custom colors in picker (close #127)

--- app/ui/ColorPickerPopup.jsx
+++ app/ui/ColorPickerPopup.jsx
@@ -1,12 +1,26 @@
 import "ui/ColorPickerPopup.styl";
 
-import { h, Component } from "preact";
+import { h, Component, createRef } from "preact";
 
 import consts from "consts.js";
 
 export default class ColorPickerPopup extends Component {
+	constructor(props) {
+		super(props);
+
+		this.customColorPicker = createRef();
+	}
+
 	pickColor(color) {
 		this.props.selectColor(color);
+	}
+
+	pickCustomColor(event) {
+		this.props.selectColor(event.target.value.substr(1)); //we need to get rid of the #
+	}
+
+	openCustomPicker() {
+		this.customColorPicker.current.click();
 	}
 
 	render(props, state) {
@@ -18,8 +32,20 @@ export default class ColorPickerPopup extends Component {
 			/>;
 		});
 
+		const isCustomColor = !consts.colors.includes(props.value);
+
 		return <div class="pickerPopup colorPickerPopup">
 			{colorElements}
+			<div
+				class={`colorPickerPopupColor customColorSwatch ${isCustomColor ? "selected" : ""}`}
+				style={isCustomColor ? `background-color: ${props.value}` : ""}
+				onClick={this.openCustomPicker.bind(this)}>
+				•••
+				<input type="color"
+					ref={this.customColorPicker}
+					onChange={this.pickCustomColor.bind(this)}
+					class="customColor" />
+			</div>
 		</div>;
 	}
 };
