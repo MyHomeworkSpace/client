85804f1c6c3ba6b5a04df8233f85876e004f1f86
William Barkoff <william@barkoffusa.com>
2020-10-03 18:32:49 -0400

    dashboard: say "a week from tomorrow" instead of day name

--- app/utils.js
+++ app/utils.js
@@ -19,8 +19,10 @@ export function relativeDate(due) {
 		dueText = dueText[0].toLowerCase() + dueText.substr(1);
 	}
 
-	if (daysTo > 7 && daysTo < 14) {
+	if (daysTo > 8 && daysTo < 14) {
 		dueText = "a week from " + due.format("dddd");
+	} else if (daysTo == 8) {
+		dueText = "a week from tomorrow";
 	} else if (daysTo == 7) {
 		dueText = "a week from today";
 	}
