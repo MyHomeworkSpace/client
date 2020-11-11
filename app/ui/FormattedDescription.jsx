83761e70140c0c99d04c69da8a96261342d18c21
Alex Studer <alex@studer.dev>
2020-11-11 11:47:59 -0500

    ui: detect and link to any URLs in homework or calendar event descriptions

--- app/ui/FormattedDescription.jsx
+++ app/ui/FormattedDescription.jsx
@@ -4,7 +4,32 @@ export default class FormattedDescription extends Component {
 	render(props, state) {
 		return <div>
 			{props.text.split("\n").map((line) => {
-				return <div>{line}</div>;
+				const linkRegex = /(http[s]*:\/\/.*?)($|\s)/ig;
+
+				const parts = [];
+				var index = 0;
+
+				while (true) {
+					const part = linkRegex.exec(line);
+
+					if (!part) {
+						// we're done
+						parts.push(<span>{line.substring(index)}</span>);
+						break;
+					}
+
+					// add the part from before this match up to this match
+					parts.push(<span>{line.substring(index, part.index)}</span>);
+
+					// add the link
+					const url = part[1];
+					parts.push(<a href={url} target="_blank" rel="noopener noreferrer">{url}</a>);
+
+					// continue
+					index = part.index + url.length;
+				}
+
+				return <div>{parts}</div>;
 			})}
 		</div>;
 	}
