377567d676ff0ba8a94c4fb721315351d1f1312b
Alex Studer <alex@studer.dev>
2021-01-24 19:00:20 -0500

    admin: make feedback/announcement items into divs, not tr and td, since they aren't in a table

--- app/admin/panes/feedback/Feedback.jsx
+++ app/admin/panes/feedback/Feedback.jsx
@@ -23,14 +23,12 @@ export default class Feedback extends Component {
 	}
 
 	render(props, state) {
-		return <tr class="feedback-item">
-			<td>
-				<h4>[<code>{String(props.feedback.id).padStart(4, "0")}</code>] <i class={`fa fa-fw ${this.typeToIcon(props.feedback.type)}`}></i> {props.feedback.text}</h4>
-				<p class="attribute"><strong>Type</strong>: {props.feedback.type}</p>
-				<p class="attribute"><strong>Timestamp</strong>: {new Date(props.feedback.timestamp).toLocaleString()}</p>
-				<p class="attribute"><strong>User</strong>: {props.feedback.userName} &lt;{props.feedback.userEmail}&gt;</p>
-				<p class="attribute"><strong>Screenshot</strong>: {props.feedback.hasScreenshot ? <a href={api.buildURL(`admin/getFeedbackScreenshot/${props.feedback.id}`, "GET", {})} rel="noreferrer noopener" target="_blank">Open screenshot</a> : "No screenshot."}</p>
-			</td>
-		</tr >;
+		return <div class="feedback-item">
+			<h4>[<code>{String(props.feedback.id).padStart(4, "0")}</code>] <i class={`fa fa-fw ${this.typeToIcon(props.feedback.type)}`}></i> {props.feedback.text}</h4>
+			<p class="attribute"><strong>Type</strong>: {props.feedback.type}</p>
+			<p class="attribute"><strong>Timestamp</strong>: {new Date(props.feedback.timestamp).toLocaleString()}</p>
+			<p class="attribute"><strong>User</strong>: {props.feedback.userName} &lt;{props.feedback.userEmail}&gt;</p>
+			<p class="attribute"><strong>Screenshot</strong>: {props.feedback.hasScreenshot ? <a href={api.buildURL(`admin/getFeedbackScreenshot/${props.feedback.id}`, "GET", {})} rel="noreferrer noopener" target="_blank">Open screenshot</a> : "No screenshot."}</p>
+		</div>;
 	}
 };
