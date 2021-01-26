577f8fb484cfd38347873a1b315c4d6715fb703b
William Barkoff <william@barkoffusa.com>
2021-01-25 19:36:41 -0500

    feedback: display UA

--- app/admin/panes/feedback/Feedback.jsx
+++ app/admin/panes/feedback/Feedback.jsx
@@ -28,6 +28,7 @@ export default class Feedback extends Component {
 			<p class="attribute"><strong>Type</strong>: {props.feedback.type}</p>
 			<p class="attribute"><strong>Timestamp</strong>: {new Date(props.feedback.timestamp).toLocaleString()}</p>
 			<p class="attribute"><strong>User</strong>: {props.feedback.userName} &lt;{props.feedback.userEmail}&gt;</p>
+			<p class="attribute"><strong>User Agent</strong>: {props.feedback.userAgent || "No user agent."}</p>
 			<p class="attribute"><strong>Screenshot</strong>: {props.feedback.hasScreenshot ? <a href={api.buildURL(`admin/getFeedbackScreenshot/${props.feedback.id}`, "GET", {})} rel="noreferrer noopener" target="_blank">Open screenshot</a> : "No screenshot."}</p>
 		</div>;
 	}
