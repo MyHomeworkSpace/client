02d8ce7f9975b7cb7db80aa73b6f851a109bda7a
Alex Studer <alex@studer.dev>
2021-02-03 21:40:06 -0500

    settings/applications: fix typos in MyApplicationDeleteModal, and convert let -> const

--- app/settings/panes/applications/MyApplicationDeleteModal.jsx
+++ app/settings/panes/applications/MyApplicationDeleteModal.jsx
@@ -33,14 +33,14 @@ export default class MyApplicationDeleteModal extends Component {
 	}
 
 	render(props, state) {
-		let confirmPhrase = "delete my application";
+		const confirmPhrase = "delete my application";
 
 		return <Modal title="Delete application" openModal={props.openModal} close={this.close.bind(this)}>
 			<div class="modal-body myApplicationDeleteModal">
 				<div className="alert alert-danger"><strong>Read this entire message.</strong> Deleting this application may have unintended side effects. There is no confirmation screen after this dialog.</div>
 				<p>You are deleting the application <strong>{props.modalState.application.name}</strong> (Application ID: <code>{props.modalState.application.id}</code>). <u>This action cannot be undone.</u></p>
-				<p>Your Client ID and each of your authorization tokens will immediately be revoked, and your application's connection to MyHomeworkSpace will cease to function. You will be unable to recieve the same application ID again.</p>
-				<p>If you understand these concequences and wish to continue, type <strong class="no-select">{confirmPhrase}</strong> in the box below, then click "Delete."</p>
+				<p>Your Client ID and each of your authorization tokens will immediately be revoked, and your application's connection to MyHomeworkSpace will cease to function. You will be unable to receive the same application ID again.</p>
+				<p>If you understand these consequences and wish to continue, type <strong class="no-select">{confirmPhrase}</strong> in the box below, then click "Delete."</p>
 				<input type="text" placeholder={confirmPhrase} class="form-control" onInput={linkState(this, "confirmPhrase")} />
 			</div>
 			<div class="modal-footer">
