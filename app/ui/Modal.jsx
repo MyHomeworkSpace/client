ff42f615b41f62c4a6dcccfb500cc64ee228ca40
Alex Studer <alex@studer.dev>
2021-08-19 02:47:58 -0400

    ui/Modal: use the absolute mouse position instead of relative movement for dragging
    this helps fix an issue where, if you moved the mouse fast enough, the js
    couldn't keep up and the modal and mouse positions would get separated

--- app/ui/Modal.jsx
+++ app/ui/Modal.jsx
@@ -1,22 +1,24 @@
 import "ui/Modal.styl";
 
 import { h } from "preact";
-import { useState, useEffect } from "preact/hooks";
+import { useState, useEffect, useRef } from "preact/hooks";
 
 export default function Modal(props) {
 	const [marginLeft, setMarginLeft] = useState(0);
-	const [marginTop, setMarginTop] = useState(0);
+	const [marginTop, setMarginTop] = useState(30);
+
+	const [dragOffsetLeft, setDragOffsetLeft] = useState(0);
+	const [dragOffsetTop, setDragOffsetTop] = useState(0);
 	const [isDragging, setIsDragging] = useState(false);
+	const modalDragWrapper = useRef(null);
 
 	useEffect(() => {
-		const move = (x, y) => {
-			setMarginLeft(marginLeft + x);
-			setMarginTop(marginTop + y);
-		};
-
-		const mouse = ({ movementX, movementY, buttons }) => {
+		const mouse = ({ clientX, clientY, buttons }) => {
 			if (isDragging) {
-				move(movementX, movementY);
+				const modalDragWrapperPosition = modalDragWrapper.current.getBoundingClientRect();
+
+				setMarginLeft(clientX - dragOffsetLeft - (window.innerWidth / 2) + (modalDragWrapperPosition.width / 2));
+				setMarginTop(clientY - dragOffsetTop);
 			}
 
 			if (buttons === 0) {
@@ -31,15 +33,33 @@ export default function Modal(props) {
 		};
 	});
 
+	const mouseDown = (e) => {
+		const modalDragWrapperPosition = modalDragWrapper.current.getBoundingClientRect();
+
+		setIsDragging(true);
+		setDragOffsetLeft(e.clientX - modalDragWrapperPosition.left);
+		setDragOffsetTop(e.clientY - modalDragWrapperPosition.top);
+	};
+
+	const mouseUp = () => {
+		setIsDragging(false);
+	};
+
 	return <div
 		class={`modal modal-preact ${!props.noClose && "noClose"} ${props.class ? props.class : ""}`}
-		style="display: block; padding-left: 0px;">
+		style="display: block; padding-left: 0px;"
+	>
 		<div class="modal-dialog" role="document">
-			<div class="modal-drag-wrapper" style={`margin: ${marginTop}px ${-marginLeft}px ${-marginTop}px ${marginLeft}px;`}>
+			<div
+				class="modal-drag-wrapper"
+				style={`margin: ${marginTop}px ${-marginLeft}px ${-marginTop}px ${marginLeft}px;`}
+				ref={modalDragWrapper}
+			>
 				<div class="modal-content modal-animation">
 					<div class="modal-header"
-						onMouseDown={() => setIsDragging(true)}
-						onMouseUp={() => setIsDragging(false)}>
+						onMouseDown={mouseDown}
+						onMouseUp={mouseUp}
+					>
 						{!props.noClose && <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick={() => props.openModal("")}><span aria-hidden="true">&times;</span></button>}
 						<h4 class="modal-title">{props.title}</h4>
 					</div>
